07e3b2cd93a07bf21a5e7d13f376de197461b5cd
Jon Savage <jon5@vt.edu>
2017-01-17 19:44:07 -0500

    babel-preset-stage-1-ify RooTokenAuthentication.jsx
    Refactor to add new sytax afforded by babel-preset-stage-1

--- app/js/components/RootTokenAuthentication.jsx
+++ app/js/components/RootTokenAuthentication.jsx
@@ -5,22 +5,16 @@ import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
 class RootTokenAuthentication extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {token: ''};
+  state = {token: ''};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
+  handleChange = (event) => {
     this.setState({token: event.target.value});
-  }
+  };
 
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     this.props.onSubmit(this.state.token);
     event.preventDefault();
-  }
+  };
 
   render() {
     return (
