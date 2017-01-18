e754eeae14fbc4a6d7527ae23f79a2db4188813c
Jon Savage <jon5@vt.edu>
2017-01-17 19:39:26 -0500

    bel-preset-stage-1-ify UserPassAuthentication.jsx
    Refactor to add new sytax afforded by babel-preset-stage-1

--- app/js/components/UserPassAuthentication.jsx
+++ app/js/components/UserPassAuthentication.jsx
@@ -5,30 +5,23 @@ import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
 class UserPassAuthentication extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: ''
-    };
+  state = {
+    username: '',
+    password: ''
+  };
 
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleUsernameChange(event) {
+  handleUsernameChange = (event) => {
     this.setState({username: event.target.value});
-  }
+  };
 
-  handlePasswordChange(event) {
+  handlePasswordChange = (event) => {
     this.setState({password: event.target.value});
-  }
+  };
 
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     this.props.onSubmit(this.state.username, this.state.password);
     event.preventDefault();
-  }
+  };
 
   render() {
     return (
