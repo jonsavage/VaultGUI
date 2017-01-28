b7be5d0eccc0a70672ac67eff6f094a8f1a141da
Jon Savage <jon5@vt.edu>
2017-01-28 14:03:14 -0500

    Improve Mounts Page
    Previously, the Mounts page would constantly hit the server to update
    the auth and mounts. This change fixes that by passing the promises used
    to get those values so that the Mounts component can handle them itself.

--- app/js/components/Mounts.jsx
+++ app/js/components/Mounts.jsx
@@ -5,14 +5,41 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 
 class Mounts extends React.Component {
 
+  componentWillMount() {
+    this.loadMounts();
+  }
+
+  state = {
+    auths: '',
+    mounts: ''
+  }
+
+  loadAuths = () => {
+    this.props.getAuths()
+      .then((result) => {
+        this.setState({auths: JSON.stringify(result, null, 4)});
+      });
+  };
+
+  loadMounts = () => {
+    this.props.getMounts()
+      .then((result) => {
+        this.setState({mounts: JSON.stringify(result, null, 4)});
+      });
+  };
+
   render() {
     return (
       <Tabs>
-        <Tab label="Secret Backends" >
-          <textarea value={this.props.auths}></textarea>
+        <Tab
+          label="Secret Backends"
+          onActive={this.loadMounts} >
+            <textarea value={this.state.mounts}></textarea>
         </Tab>
-        <Tab label="Auth Backends" >
-          <textarea value={this.props.mounts}></textarea>
+        <Tab
+          label="Auth Backends"
+          onActive={this.loadAuths}>
+            <textarea value={this.state.auths}></textarea>
         </Tab>
       </Tabs>
     );
