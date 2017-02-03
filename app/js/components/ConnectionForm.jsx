b774126adc4ce39a012aa4387ce2282120a30b2e
Jon Savage <jon5@vt.edu>
2017-02-03 16:51:05 -0500

    Add SnaxkBar Error Message For Failed Connection
    Upon failure to connect to a vault server (on the Connection page)
    an error is now shown in the form of a `SnackBar` at the bottom of the page.

--- app/js/components/ConnectionForm.jsx
+++ app/js/components/ConnectionForm.jsx
@@ -1,25 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Snackbar from 'material-ui/Snackbar';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
 class ConnectionForm extends React.Component {
-  state = {url: ''};
+  state = {
+    url: '',
+    snackbar: {
+      open: false,
+      message: ''
+    }
+  };
 
   handleChange = (event) => {
     this.setState({url: event.target.value});
   };
 
+  handleSnackbarRequestClose = () => {
+    this.setState({
+      snackbar: {
+        open: false,
+        message: ''
+      }
+    });
+  };
+
   handleSubmit = (event) => {
     this.props.onSubmit(this.state.url)
-      .catch((err) => console.log(err));
+      .catch((err) =>
+        this.setState(
+          {
+            snackbar: {
+              open: true,
+              message: err
+            }
+          }));
 
     event.preventDefault();
   };
 
   render() {
     return (
+      <div>
         <form onSubmit={this.handleSubmit}>
           <TextField
             floatingLabelText="Server Url"
@@ -31,6 +55,12 @@ class ConnectionForm extends React.Component {
             label="Connect"
             primary={true}/>
         </form>
+        <Snackbar
+          open={this.state.snackbar.open}
+          message={this.state.snackbar.message}
+          autoHideDuration={4000}
+          onRequestClose={this.handleSnackbarRequestClose}/>
+      </div>
     );
   }
 }
